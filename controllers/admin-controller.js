1a794f4a439537677ee7e045a65e93959b1de54e
ayayam <aya.s.yamamoto@gmail.com>
2023-05-01 19:30:59 -0400

    added more changes

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const data = require('../data/data');
+const {v4: uuid} = require('uuid');
 
 module.exports = {
     admin_console: (req, res) => {
@@ -8,12 +9,17 @@ module.exports = {
     },
     create_book: (req, res) => {
         res.render('pages/create', {
-
         });
+        const { _id = uuid(), title, author, publisher, genre, pages, rating, synopsis } = request.body;
+        data.push({_id, title, author, publisher, genre, pages, rating, synopsis});
+        response.redirect('/admin-console');
     },
     update_book: (req, res) => {
+        const { _id } = request.params;
+        const { title, author, publisher, genre, pages, rating, synopsis } = request.body;
+        const foundBook = books.find(book => book._id === String(_id));
         res.render('pages/update', {
-
+            foundBook: foundBook,
         });
     }
 }
--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,17 +1,24 @@
 const data = require('../data/data');
+const {v4: uuid} = require('uuid');
 
 module.exports = {
     book_detail: (req, res) => {
+        const { _id } = req.params;
+        console.log(_id);
+        const comicbook = data.filter(book => book.id === String(_id));
+        console.log(comicbook);
         res.render('pages/book', {
-            books: data,
-            image: data.image,
-            title: data.title,
-            author: data.author,
-            publisher: data.publisher,
-            genre: data.genre,
-            pages: data.pages,
-            rating: data.rating,
-            synopsis: data.synopsis
+            books: comicbook,
+            // books: data,
+            // image: data.image,
+            // title: data.title,
+            // author: data.author,
+            // publisher: data.publisher,
+            // genre: data.genre,
+            // pages: data.pages,
+            // rating: data.rating,
+            // synopsis: data.synopsis
         });
-    }
+    },
+
 }
