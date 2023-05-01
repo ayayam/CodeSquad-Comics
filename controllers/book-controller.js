01165ea98843ccd19b12c2530a64ef209331e705
ayayam <aya.s.yamamoto@gmail.com>
2023-05-01 19:53:46 -0400

    fixed books

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -3,21 +3,13 @@ const {v4: uuid} = require('uuid');
 
 module.exports = {
     book_detail: (req, res) => {
-        const { _id } = req.params;
-        console.log(_id);
-        const comicbook = data.filter(book => book.id === String(_id));
+        // const { _id } = req.params;
+        let id = req.params._id;
+        // console.log(_id);
+        const comicbook = data.find(book => book._id === String(id));
         console.log(comicbook);
         res.render('pages/book', {
             books: comicbook,
-            // books: data,
-            // image: data.image,
-            // title: data.title,
-            // author: data.author,
-            // publisher: data.publisher,
-            // genre: data.genre,
-            // pages: data.pages,
-            // rating: data.rating,
-            // synopsis: data.synopsis
         });
     },
 
