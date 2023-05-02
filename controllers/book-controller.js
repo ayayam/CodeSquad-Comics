4451c7cf589c3e4942b3dbe400743f1dc4012497
ayayam <aya.s.yamamoto@gmail.com>
2023-05-01 20:33:28 -0400

    finished part 2 of homework-12

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -12,5 +12,31 @@ module.exports = {
             books: comicbook,
         });
     },
+    create_book_post: (req, res) => {
+        const { _id=uuid(), title, author, publisher, genre, pages, rating, synopsis } = request.body;
+        data.push({_id, title, author, publisher, genre, pages, rating, synopsis});
+        res.redirect('/admin-console');
+    },
+    book_update_put: (req, res) => {
+        const {_id} = request.params;
+        const {title, author, publisher, genre, pages, rating, synopsis} = request.body;
+        const foundBook = data.find(book => book._id === String(_id));
 
+        foundBook.title = title;
+        foundBook.author = author;
+        foundBook.publisher = publisher;
+        foundBook.genre = genre;
+        foundBook.pages = pages;
+        foundBook.rating = rating;
+        foundBook.synopsis = synopsis
+
+        res.redirect('/admin-console');
+    },
+    book_delete: (req, res) => {
+        const { _id } = request.params;
+        const foundBook = data.find(book => book._id === String(_id));
+        const index = data.indexOf(foundBook);
+        data.splice(index, 1);
+        response.redirect('admin-console');
+    }
 }
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/book-controller');
 
+router.route('/')
+    .post(bookController.create_book_post)
 
 router.route('/:_id')
     .get(bookController.book_detail)
+    .put(bookController.book_update_put)
+    .delete(bookController.book_delete)
+
+    // .post connected to create method
 
 module.exports = router;
