67e4bcbf25c30d610321b381b919e2a3a3479e67
ayayam <aya.s.yamamoto@gmail.com>
2023-05-18 11:09:14 -0400

    created homework-15 branch, completed up to section 4

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 const path = require('path');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
+
 
 app.set('view engine', 'ejs');
 
@@ -11,8 +15,17 @@ app.use(morgan('combined'));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended:true }));
+
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
 
@@ -22,6 +35,9 @@ require('./config/connection');
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
   });
+
+
+
 // app.get('/', (req, res) => {
 //     res.render('pages/index', {
 
---
+++ models/user-model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongooseFindOrCreate = require('mongoose-findorcreate');
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    googleId: {
+        type: String,
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(mongooseFindOrCreate);
+
+const User = mongoose.model('users', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+        cb(null, { id: user.id, username: user.username, name: user.displayName });
+    });
+});
+
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL:"http://localhost:3000/auth/google/comics"
+},
+function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleID: profile.id }, function(err, user) {
+        return cb(err, user);
+    });
+}));
+
+module.exports = User;
